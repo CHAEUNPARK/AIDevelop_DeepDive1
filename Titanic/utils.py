10f8a9028b812b5ab6928bc7b23a1898a0baecea
CHAEUN PARK <codjs0910@naver.com>
2019-07-22 19:52:20 +0900

    Titanic model import

---
+++ Titanic/utils.py
@@ -0,0 +1,7 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
