293a7a2cc675ce83e4fd2fa4e6dc3f8e55525d55
CHAEUN PARK <codjs0910@naver.com>
2019-07-22 15:57:25 +0900

    data load

--- CreditCard/creditcard.py
+++ CreditCard/creditcard.py
@@ -4,3 +4,16 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
+
+# 데이터 로드
+data = pd.read_csv('./CreditCard/creditcard.csv')
+print(data.head())
+print(data.columns) #컬럼명 확인
+# 데이터 빈도수 확인
+print(pd.value_counts(data['Class']))
+
+pd.value_counts(data['Class']).plot.bar()
+plt.title('Fraud class histogram')
+plt.xlabel('Class')
+plt.ylabel('Frequency')
+plt.show()
