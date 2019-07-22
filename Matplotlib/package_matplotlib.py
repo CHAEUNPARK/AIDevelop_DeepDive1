892e8c4543ad015fcb45844d0358e07902162399
CHAEUN PARK <codjs0910@naver.com>
2019-07-22 19:57:58 +0900

    sin cos

--- Matplotlib/package_matplotlib.py
+++ Matplotlib/package_matplotlib.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt
+import numpy as np
+def plot():
+    plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+    plt.show()
 
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-plt.show()
+def plot1():
+    X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+    C, S = np.cos(X), np.sin(X)
+
+    pt1 = plt.plot(X, C)
+    pt2 = plt.plot(X, S)
+    print(pt1, pt2)
+
+    plt.show()
+
+if __name__ == '__main__':
+    # plot()
+    # plot1()
+    pass
